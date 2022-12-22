2a26d7b4666b54df1b04b6a20ceb3027821a4bfb
Yuriko <ygabriela.arnott@gmail.com>
2022-12-22 17:30:01 -0500

    1222 update

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import Home from "./components/Home"
 import './App.css';
+import { Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Switch>
+        <Route exact path = '/'>
+          <Home/>
+        </Route>
+        <Route exact path = '/main'>
+          <h1>gfdgdgf</h1>
+        </Route>
+      </Switch>
   );
 }
 
---
+++ client/src/components/Home.js
@@ -0,0 +1,9 @@
+function Home(){
+    return (
+ <div>
+    hello world
+ </div>
+    )
+}
+
+export default Home
