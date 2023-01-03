11c0f468587119a4d034a45e006bc79fc05a1a48
Pablo Fuentes Tudela <pablo.fuentes.tudela@outlook.com>
2023-01-03 17:16:05 -0500

    "added front end post login request"
    Co-authored-by: yuriko021@hotmail.com <yuriko021@hotmail.com>
    Co-authored-by: Isaac Jaquez <Isaacjqz@users.noreply.github.com>

--- client/src/components/user/SignIn.js
+++ client/src/components/user/SignIn.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom'
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -16,9 +17,46 @@ import NavBar from '../NavBar/NavBar';
 const theme = createTheme();
 
 const SignIn = () => {
+  const [formData, setFormData] = useState ({
+    name:'',
+    email:'',
+    password:''
+  }) //need form
+  const [errors, setErrors] = useState([])
+  const history = useHistory()
+  const {name, email, password} = formData
     const handleSubmit = (event) => {
         event.preventDefault();
+        const user = {
+          name,
+          password
+        }
+        fetch('/signin', {
+          method: 'POST',
+          headers:{'Content-Type': 'application/json'},
+          body:JSON.stringify(user)
+        })
+        .then(res => {
+          if(res.ok){
+            res.json().then(user => {
+              history.push(`/users/${user.id}`)
+            })
+          }else {
+            res.json().then(json => setErrors(Object.entries(json.errors)))
+          }
+        })
       };
+      const handleChange = (event) => {
+        const {name, value} = event.target
+        setFormData({...formData, [name]: value})
+      }
+
+
+
+// const SignIn = () => {
+//     const handleSubmit = (event) => {
+//         event.preventDefault();
+//       };
 
 // const signIn = {
 
